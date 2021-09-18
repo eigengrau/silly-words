17e4ba060d7dfa39d21b032d57a4c91792d16cd2
Sebastian Reuße <seb@schattenkopie.de>
2021-09-18 18:41:12 +0200

    build(nix): prefer passing dependencies explicitly

--- nix/silly-words.nix
+++ nix/silly-words.nix
@@ -1,7 +1,7 @@
-{ pkgs }:
-pkgs.python38Packages.buildPythonPackage {
+{ python3Packages }:
+python3Packages.buildPythonPackage {
   pname = "silly-words";
   version = "0.1.0.0";
-  propagatedBuildInputs = (with pkgs.python38Packages; [ pyparsing nltk ]);
+  propagatedBuildInputs = (with python3Packages; [ pyparsing nltk ]);
   src = ../.;
 }
